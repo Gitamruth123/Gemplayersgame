a0b165885f90ba3f834e152f733270d7e3e8bb38
Gitamruth123 <amruthav1991@gmail.com>
2024-06-12 18:58:04 -0400

    game board

--- Gemplayersgame/Program.cs
+++ Gemplayersgame/Program.cs
@@ -26,22 +26,65 @@ public class Player
     }
     public void Move(char direction)
     {
-        switch (direction)
+        switch(direction)
         {
-            case 'U':
-                if (Position.Y > 0) Position.Y -= 1;
+           case 'U':
+               if(Position.Y > 0) Position.Y -= 1;
                 break;
-            case 'D':
-                if (Position.Y < 5) Position.Y += 1;
+           case 'D':
+               if(Position.Y < 5) Position.Y += 1;
                 break;
-            case 'L':
-                if (Position.X > 0) Position.X -= 1;
+           case 'L':
+               if(Position.X > 0) Position.X -= 1;
                 break;
-            case 'R':
-                if (Position.X < 5) Position.X += 1;
+           case 'R':
+               if(Position.X < 5) Position.X += 1;
                 break;
         }
 
     }
 
+}
+public class Board
+{
+    private const int Size = 6;
+    private string[,] grid;
+    private Random random;
+
+    public Board()
+    {
+        grid = new string[Size, Size];
+        random = new Random();
+        InitializeGrid();
+    }
+
+    private void InitializeGrid()
+    {
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                grid[i, j] = "-";
+
+       PlaceItem("P1", 0, 0);
+       PlaceItem("P2", 5, 5);
+       PlaceRandomItems("G", 5);
+       PlaceRandomItems("O", 5);
+    }
+    private void PlaceItem(string item, int x, int y)
+    {
+       grid[y, x] = item;
+    }
+
+    private void PlaceRandomItems(string item, int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            int x, y;
+            do
+            {
+               x = random.Next(Size);
+               y = random.Next(Size);
+            } while(grid[y, x] != "-");
+            grid[y, x] = item;
+        }
+    }
 }
